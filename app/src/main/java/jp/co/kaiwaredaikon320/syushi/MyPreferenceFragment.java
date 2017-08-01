1d5f50f74414ea9db2e16eb62f2bd04356989356
かいわれ <kaiware@kaiware-no-MacBook-Air.local>
2017-08-01 23:42:14 +0900

    170801＿統合＿Ver0.0.1

--- app/src/main/java/jp/co/kaiwaredaikon320/syushi/MyPreferenceFragment.java
+++ app/src/main/java/jp/co/kaiwaredaikon320/syushi/MyPreferenceFragment.java
@@ -28,8 +28,6 @@ public class MyPreferenceFragment extends PreferenceFragment implements SharedPr
     CharSequence keyId_tani;
 
     CharSequence keyId_destination;
-	CharSequence keyId_destination2;
-	CharSequence keyId_destination3;
 
 
     @SuppressLint("NewApi")
